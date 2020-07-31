97f371ad6d1331b5daf85b103199716cf7fc4f39
Daniel Freitas <dfreitas@asert.com.br>
2020-07-31 11:22:30 -0300

    Projeto criado

---
+++ src/main/java/com/danielfreitasbs/cursomcspring/CursomcSpringApplication.java
@@ -0,0 +1,13 @@
+package com.danielfreitasbs.cursomcspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielfreitasbs/cursomcspring/CursomcSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielfreitasbs.cursomcspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
