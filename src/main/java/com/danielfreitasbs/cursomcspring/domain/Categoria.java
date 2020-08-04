7cae6b55697a3e59191fcc467455b9d9260b0882
Daniel Freitas <daniel.italofreitas@gmail.com>
2020-08-03 21:19:26 -0300

    Banco de dados h2 e criação automatica de base de dados

--- src/main/java/com/danielfreitasbs/cursomcspring/domain/Categoria.java
+++ src/main/java/com/danielfreitasbs/cursomcspring/domain/Categoria.java
@@ -1,8 +1,16 @@
 package com.danielfreitasbs.cursomcspring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
