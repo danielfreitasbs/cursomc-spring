a619a2cebbe07bb8491b8f1dca5d6797616455bc
Daniel Freitas <daniel.italofreitas@gmail.com>
2020-08-03 21:52:49 -0300

    Criando operacao de instanciacao

--- src/main/java/com/danielfreitasbs/cursomcspring/CursomcSpringApplication.java
+++ src/main/java/com/danielfreitasbs/cursomcspring/CursomcSpringApplication.java
@@ -1,13 +1,32 @@
 package com.danielfreitasbs.cursomcspring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcSpringApplication {
+import com.danielfreitasbs.cursomcspring.domain.Categoria;
+import com.danielfreitasbs.cursomcspring.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcSpringApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+	
+		this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
